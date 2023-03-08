30fadaeaa8f8b4ae1c293b1c2e89452b68ca0935
Leandro Melloni Valle <lmvalle01@hotmail.com>
2023-03-08 20:59:58 -0300

    teste deploy

--- dist/index.js
+++ dist/index.js
@@ -2501,6 +2501,12 @@ async function run() {
     const technology = core.getInput('technology', { required: true });
     const awsFunction = core.getInput('aws-function', { required: true });
 
+    if (technology == 'iac' && awsFunction == 'terraform' || technology == 'iac' && awsFunction == 'cloudformation') {
+      console.log('Valid technology and aws-function inputs'); 
+    } else {    
+        throw new Error('Invalid technology or aws-function input');
+    }
+
     // Get the function name
     core.setOutput('aws-function', awsFunction);
     console.log("A tecnologia usada e: " + technology);
