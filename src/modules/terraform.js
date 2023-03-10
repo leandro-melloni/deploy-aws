5052d3bdaf190c218b1a212f55230ba2954aa872
Leandro Melloni Valle <lmvalle01@hotmail.com>
2023-03-10 12:20:03 -0300

    teste

--- src/modules/terraform.js
+++ src/modules/terraform.js
@@ -1,8 +1,8 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-async function lsWithGrep() {
+function lsWithGrep() {
   try {
-      const { stdout, stderr } = await exec('ls | grep js');
+      const { stdout, stderr } = exec('ls | grep js');
       console.log('stdout:', stdout);
       console.log('stderr:', stderr);
   }catch (err) {
