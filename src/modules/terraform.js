41b84209b1ce42de7663236e485dc36b6ef332b2
Leandro Melloni Valle <lmvalle01@hotmail.com>
2023-03-10 20:11:28 -0300

    teste

--- src/modules/terraform.js
+++ src/modules/terraform.js
@@ -5,8 +5,8 @@ const exec = util.promisify(require('child_process').exec);
 async function invokeTerraform() {
   try {
       const { stdout, stderr } = await exec('terraform init');
-      //console.log('stdout:', stdout);
-      //console.log('stderr:', stderr);
+      console.log('stdout:', stdout);
+      console.log('stderr:', stderr);
       return stdout, stderr;
   }catch (err) {
       throw new Error(err);
